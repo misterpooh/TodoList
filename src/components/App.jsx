ae4c267393f33fcbe7b9dfeb2acfbab8edd17371
misterpooh <33590996+misterpooh@users.noreply.github.com>
2022-07-06 01:03:52 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Item from "./Item";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState("");
+  function changeItem(event) {
+    setItem(event.target.value);
+  }
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+    return;
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={changeItem} value={item} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        {items.map((todo) => (
+          <Item item={todo} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Item(props) {
+  return (
+    <ul>
+      <li>{props.item}</li>
+    </ul>
+  );
+}
+
+export default Item;
